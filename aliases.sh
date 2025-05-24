f6d1a05eb42ddefe4e4164c3d0fdc2a477397a42
MaxProger338 <maxproger338@proton.me>
2025-05-25 02:34:09 +0700

    fix: If you don't select anything in FZF, then VIM will open its sart page

--- aliases.sh
+++ aliases.sh
@@ -8,7 +8,7 @@
 # Default vim
 alias v=". $HOME/.local/bin/vimim.sh"
 # Vim with fzf
-alias vf='. $HOME/.local/bin/vimim.sh $(fzf)'
+alias vf='. $HOME/.local/bin/vimim.sh -fzf'
 alias vv=". $HOME/.local/bin/vimim.sh ."
 
 # For use vim with root-permisstions (replace doas with sudo)
--- vimim.sh
+++ vimim.sh
@@ -3,6 +3,11 @@
 # If param is empty, use prev vim param (from $VIM_PREV)
 if [[ "$#" -eq 0 ]]; then
 	vim $VIM_PREV
+elif [[ "$1" = "-fzf" ]]; then
+	dir=$(fzf)
+	if [[ ! -z "$dir" ]]; then
+		vim "$dir"
+	fi
 else
 	[[ "$1" != "." ]] && export VIM_PREV="$1"  # not require
 	vim "$1"
