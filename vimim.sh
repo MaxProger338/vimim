64bb0619f0c069b3683325c1fe06bd8729dd42eb
MaxProger338 <maxproger338@proton.me>
2025-05-25 02:21:49 +0700

    ref: Added "" near variables

--- vimim.sh
+++ vimim.sh
@@ -4,6 +4,6 @@
 if [[ "$#" -eq 0 ]]; then
 	vim $VIM_PREV
 else
-	[[ $1 != "." ]] && export VIM_PREV="$1"  # not require
-	vim $1
+	[[ "$1" != "." ]] && export VIM_PREV="$1"  # not require
+	vim "$1"
 fi
